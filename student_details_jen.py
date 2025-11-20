92b0d32b47772fd08e52b1ff577b62010cfe8cea
Snehalata-Goni-11 <snehagoni60@gmail.com>
2025-11-20 16:09:05 +0530

    Update student_details_jen.py

--- student_details_jen.py
+++ student_details_jen.py
@@ -4,10 +4,10 @@ if len(sys.argv) != 3:
   print("Usage:python studnet.py<name><rollno>")
   sys.exit(1)
   #sys.argv[0] is always the prgram name
-  script_name=sys.argv[0]
-  name=sys.argv[1]
-  rollno=sys.argv[2]
+script_name=sys.argv[0]
+name=sys.argv[1]
+rollno=sys.argv[2]
   #printing statements
-  print("script Name:",script_name)
-  print("script student name:",name)
-  print("RollNumber:",rollno)
+print("script Name:",script_name)
+print("script student name:",name)
+print("RollNumber:",rollno)
