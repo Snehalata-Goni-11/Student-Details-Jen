650178afc67b59c7c99d8bb25588789e5d5d3902
Snehalata-Goni-11 <snehagoni60@gmail.com>
2025-11-20 14:52:53 +0530

    Update student_details_jen.py

--- student_details_jen.py
+++ student_details_jen.py
@@ -1,8 +1,13 @@
-import sys
-script_name=sys.argv[0]
-name=sys.argv[1]
-rollno = sys.argv[2]
-
-print("Script Name:",script_name)
-print("Student Name:",name)
-print("Roll Number:",rollno)
+import sys 
+    #check if correct number og argumnets 
+if len(sys.argv) != 3:
+  print("Usage:python studnet.py<name><rollno>")
+  sys.exit(1)
+  #sys.argv[0] is always the prgram name
+  script_name=sys.argv[0]
+  name=sys.argv[1]
+  rollno=sys.argv[2]
+  #printing statements
+  print("script Name:",script_name)
+  print("script student name:",name)
+  print("RollNumber:",rollno)
